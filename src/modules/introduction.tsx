e40959f258107c9feb18437786a2a87bb161c896
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-04-25 20:47:39 +0200

    fix: update content from vegard

--- src/modules/introduction.tsx
+++ src/modules/introduction.tsx
@@ -8,7 +8,13 @@ const TitleModule: React.FC = () => {
   return (
     <G3TextModule header={"Introduksjon"} skin={"purple"}>
       <>
-        <Paragraph>Her skriver vi introduksjonen</Paragraph>
+        <Paragraph>
+          Vi har i samarbeid med Terratec utviklet en løsning på deteksjon av
+          veg mar- kering hvor vi skal detektere og vurdere tilstanden på
+          gangfelt på Norges veger. Dette skal da gjennom Terratec sin
+          registeringsklient MapSpace registreres i nasjo- nal vegdatabank
+          (NVDB) og videre bli brukt til å planlegge vedlikehold av vegene.
+        </Paragraph>
       </>
     </G3TextModule>
   );
--- src/modules/methods.tsx
+++ src/modules/methods.tsx
@@ -6,16 +6,19 @@ const Methods: React.FC = () => {
     <G3TextModule header={"Metoder"} skin={"green"}>
       <>
         <p>
-          Terratec har samlet inn data fra fylkesveien i agder. Denne dataen har
+          Terratec har samlet inn data fra fylkesveien i Agder. Denne dataen har
           de prosessert og laget raster av. Rasteret inneholder detaljerte
-          gråtone-bilder og detaljert posionsdata for hver eneste piksel i
-          bildet. Vi har brukt OpenCV for å detektere elementer et
-          fotgjengerfelt består av og justert algoritmer til å gjenkjenne
-          mønstre med gjentakende elementer som bygger et samlet fotgjengerfelt.
-          Når et felt er oppdaget har vi beregnet en nøyaktig posisjon på
-          sentert av dette, som kan sendes til NVDB. Per i dag sender vi ikke
-          data til NVDB da dette steget er avhengig av detaljert informasjon man
-          får med tilgang til NVDB, som er informasjon vi har klart å få tak i.
+          gråtone-bilder og detaljert posisjonsdata for hver eneste piksel i
+          bildet. Vi har brukt computer vision biblioteket OpenCV for å
+          detektere elementer et fotgjengerfelt består av og justert algoritmer
+          til å gjenkjenne mønstre med gjentakende elementer som bygger et
+          samlet fotgjengerfelt. Når et felt er oppdaget har vi beregnet en
+          nøyaktig posisjon på senteret av dette. Videre kan dette brukes
+          gjennom Terratecs registreringsplatform MapSpace til å registrere
+          fotgjengerfeltene som objekter i NVDB som er den nasjonale
+          vegdatabasen i Norge. Informasjonen her brukes blant annet til
+          planlegging av vedlikehold, kartløsninger og ruteberegninger, og som
+          grunnlagsdata for støyberegninger og trafikkmodeller.
         </p>
         <p>
           <em>
