04752b6f1fb528841a4fbb1451914b652d0c8a16
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2022-04-25 14:59:50 +0200

    chore: minor update to methods

--- src/modules/methods.tsx
+++ src/modules/methods.tsx
@@ -8,12 +8,14 @@ const Methods: React.FC = () => {
         <p>
           Terratec har samlet inn data fra fylkesveien i agder. Denne dataen har
           de prosessert og laget raster av. Rasteret inneholder detaljerte
-          gråtone-bilder og detaljert poisionsdata for hver eneste piksel i
+          gråtone-bilder og detaljert posionsdata for hver eneste piksel i
           bildet. Vi har brukt OpenCV for å detektere elementer et
-          fotgjengerfelt består av og tunet algoritmer til å gjenkjenne mønstre
-          med gjentakende elementer som bygger et samlet fotgjengerfelt. Når et
-          felt er oppdaget har vi beregnet en nøyaktig posisjon på sentert av
-          dette, som kan sendes til NVDB.
+          fotgjengerfelt består av og justert algoritmer til å gjenkjenne
+          mønstre med gjentakende elementer som bygger et samlet fotgjengerfelt.
+          Når et felt er oppdaget har vi beregnet en nøyaktig posisjon på
+          sentert av dette, som kan sendes til NVDB. Per i dag sender vi ikke
+          data til NVDB da dette steget er avhengig av detaljert informasjon man
+          får med tilgang til NVDB, som er informasjon vi har klart å få tak i.
         </p>
         <p>
           <em>
